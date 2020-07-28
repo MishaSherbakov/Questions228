0dcc0c28767cc38880fdc3e838a57303a08025b3
MishaSherbakov <68336843+MishaSherbakov@users.noreply.github.com>
2020-07-28 11:29:26 +0300

    Add files via upload

--- lection2.cpp
+++ lection2.cpp
@@ -1,23 +1,100 @@
+ #include <iostream>
+ 
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-
-#include <iostream>
-#include <vector>
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* Output = l1;
+        int balance = 0;
+        ListNode WWE = ListNode(1);
+        while(l1->next != nullptr && l2->next != nullptr) {
+            if (l1->val + l2->val + balance >= 10) {
+                l1->val = (l1->val + l2->val + balance) % 10;
+                balance = 1;
+            } else {
+                l1->val = l1->val + l2->val + balance;
+                balance = 0;
+            }
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        if (l1->val + l2->val + balance >= 10) {
+            l1->val = (l1->val + l2->val + balance) % 10;
+            balance = 1;
+        } else {
+            l1->val = l1->val + l2->val + balance;
+            balance = 0;
+        }
+        if (l1->next != nullptr && l2->next == nullptr) {
+            l1 = l1->next;
+            while(balance != 0 && l1->next != nullptr) {
+                if (l1->val + balance >= 10) {
+                    l1->val = 0;
+                    l1 = l1->next;
+                } else {
+                    l1->val = l1->val + balance;
+                    balance = 0;
+                    break;
+                } 
+            }
+            if (l1->val + balance >= 10 && l1->next == nullptr) {
+                l1->val = 0;
+                balance = 1;
+            } else {
+                l1->val = l1->val + balance;
+                balance = 0;
+            }
+            if (balance == 1) {
+                l1->next = WWW;
+            }
+            return Output;
+        }
+        if (l1->next == nullptr && l2->next != nullptr) {
+            l1 = l2->next;
+            while(balance != 0 && l1->next != nullptr) {
+                if (l1->val + balance >= 10) {
+                    l1->val = 0;
+                    l1 = l1->next;
+                } else {
+                    l1->val = l1->val + balance;
+                    balance = 0;
+                    break;
+                } 
+            }
+            if (l1->val + balance >= 10 && l1->next == nullptr) {
+                l1->val = 0;
+                balance = 1;
+            } else {
+                l1->val = l1->val + balance;
+                balance = 0;
+            }
+            if (balance == 1) {
+                l1->next = WWW;
+            }
+            return Output;
+        }
+        if (l1->next == nullptr && l2->next == nullptr) {
+            if (balance == 1) {
+                l1->next = WWW;
+                return Output;
+            }
+            return Output;
+        }
+        return Output;
+    }
 
 int main() {
-    std::vector<int> vec;
-    int x;
-    while( std::cin >> x ) {
-        vec.push_back(x);
-    }
-    for (const auto& element : vec) {
-        std::cout << element << std::endl;
-    }
-    std::cout << x << std::endl;
-    std::cout << &x << std::endl;
-    int* addr = &x;
-    int** addr_2 = &addr;
-    *addr = 88;
-    std::cout << x << addr_2 << std::endl;
-    double* yyy = nullptr;
-    return 0;
+    ListNode l1_ = ListNode(5);
+    ListNode l2_ = ListNode(5);
+    ListNode* l1 = &l1_;
+    ListNode* l2 = &l2_;
+    ListNode* l3 = addTwoNumbers(l1, l2);
+    std::cout << l3->val << std::endl;
+    l3 = l3->next;
+    std::cout << l3->val << std::endl;
 }
